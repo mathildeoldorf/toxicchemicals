1b8b476011480e4ebd9ec0c5d2fda20a2e25570c
Mathilde Runge <maro@html24.net>
2020-05-12 20:30:20 +0200

    Toxic chemicals exam project using React, Node JS, Express, SQL, Objection, Knex and Jest: This commit includes a minimal frontend, server and database setup and a testing script using Jest

---
+++ server/config/dbcredentials.js
@@ -0,0 +1,13 @@
+module.exports = {
+    development: {
+        database: "toxicchemicals",
+        user: "root",
+        password: "Mathilde2554"
+    },
+    production: {
+        database: "toxicchemicals",
+        user: "admin",
+        password: "Mathilde2554"
+    },
+    debug: true
+}
